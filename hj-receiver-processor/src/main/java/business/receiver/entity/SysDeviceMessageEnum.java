dc17825d8ab40a20673efa5357ed42eb32544178
xudy <1922448115@qq.com>
2020-07-15 16:18:43 +0800

    删除212解析jar

--- hj-receiver-processor/src/main/java/business/receiver/entity/SysDeviceMessageEnum.java
+++ hj-receiver-processor/src/main/java/business/receiver/entity/SysDeviceMessageEnum.java
@@ -1,8 +1,7 @@
 package business.receiver.entity;
 
-import com.xy.format.hbt212.CodeMean;
 
-public enum SysDeviceMessageEnum implements CodeMean {
+public enum SysDeviceMessageEnum {
     IS_RECIEVE("0","已接收"),
     IS_JOINQEUE("1","已加入队列"),
     IS_PARSE("2","已解析"),
@@ -10,19 +9,16 @@ public enum SysDeviceMessageEnum implements CodeMean {
     private String flag;
     private String meaning;
 
-
     SysDeviceMessageEnum(String flag,String meaning){
         this.flag = flag;
         this.meaning = meaning;
     }
 
 
-    @Override
     public String code() {
         return flag;
     }
 
-    @Override
     public String mean() {
         return meaning;
     }
